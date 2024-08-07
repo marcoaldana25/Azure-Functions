0696897b974ddb47ef10cb8723673a872a8a3d76
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2024-08-07 16:30:29 -0500

    Initial add of Azure functions project w/ template example, plus custom Addition Function

---
+++ AzureFunctions/AdditionFunction.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctions
+{
+    public class AdditionFunction
+    {
+        private readonly ILogger _logger;
+
+        public AdditionFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AdditionFunction>();
+        }
+
+        [Function("Addition")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData httpRequestData)
+        {
+            var response = httpRequestData
+                .CreateResponse(System.Net.HttpStatusCode.OK);
+
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            try
+            {
+                var firstInteger = Convert.ToInt64(httpRequestData
+                    .Query["first"]);
+
+                var secondInteger = Convert.ToInt64(httpRequestData
+                    .Query["second"]);
+
+                response.WriteString($"Sum of integers in query string is: {firstInteger + secondInteger}");
+            }
+            catch (FormatException formatException)
+            {
+                var formatErrorMessage = $"Encountered an error when formatting the query string to an Int64 value: {formatException.Message}";
+
+                _logger.LogError(formatErrorMessage, formatException);
+
+                response
+                    .WriteString(formatErrorMessage);
+            }
+            catch (OverflowException overflowException)
+            {
+                var overflowErrorMessage = $"Encounted an overflow error when casting the query string to an Int64 value: {overflowException.Message}";
+
+                _logger
+                    .LogError(overflowErrorMessage, overflowException);
+
+                response
+                    .WriteString(overflowErrorMessage);
+            }
+
+            return response;
+        }
+    }
+}
